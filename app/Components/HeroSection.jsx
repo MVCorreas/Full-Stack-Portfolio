00a022b8723d005215e41673b434699a24d94a43
MVCorreas <correasmv@gmail.com>
2023-12-19 19:00:44 -0300

    NavBar

--- app/Components/HeroSection.jsx
+++ app/Components/HeroSection.jsx
@@ -32,7 +32,7 @@ const HeroSection = () => {
             />
            
           </h1>
-          <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
+          <p className='text-black text-base sm:text-lg mb-6 lg:text-xl'>
           “Nothing is impossible, the word itself says ‘ I’m possible.’ ” — Audrey Hepburn 
           </p>
           <div className='flex flex-col lg:flex-row'>
@@ -52,11 +52,11 @@ const HeroSection = () => {
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
         <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
           <Image
-            src='/Assets/portfolio.jpeg'
+            src='/ProfilePic.jpg'
             alt='portfolio'
             className='absolute rounded-full left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2'
-            width={300}
-            height={300}
+            width={500}
+            height={500}
           />
         </div>
       </div>
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import HeroSection from './Components/HeroSection'
+import Navbar from './Components/NavBar'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <Navbar />
       <HeroSection/>
     </main>
   )
