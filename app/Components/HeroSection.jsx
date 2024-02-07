a03b55d9c386ca8a18a45300b49816c01756c9c9
MVCorreas <correasmv@gmail.com>
2024-02-07 11:59:52 -0300

    Resume updated

--- app/Components/HeroSection.jsx
+++ app/Components/HeroSection.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Hire Me
             </button>
             </Link>
-            <Link href={'https://drive.google.com/file/d/1-BCMS1dMMLStVvhsiLkNun4j1TKaSDY1/view?usp=sharing'}>
+            <Link href={'https://drive.google.com/file/d/1VUJb8F3IIAdvDXwOKWhYD3MlECo69jqw/view?usp=sharing'}>
             <button className='px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-r from-yellow-500 via-amber-500 to-red-500 hover:bg-slate-800 text-white border mt-3 lg:mt-0'>
               <span className='block bg-slate-200 text-black hover:bg-slate-500 hover:text-white rounded-full px-5 py-2'>Download CV</span>
             </button>
