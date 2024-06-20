fd847670065bc0b888122db5667acb25922c8b95
Maria Victoria Correas <correasmv@gmail.com>
2024-06-20 20:24:23 -0300

    moved logo to the left

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -30,29 +30,28 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#a8dadc] bg-opacity-90 h-20">
-
-      <div className="container mx-auto flex justify-between h-full">
-        <div className="flex items-center">
-          <Image 
-            src="/Logo.png" 
-            alt="aboutImage"
-            width={110} 
-            height={110}
-            className="rounded-full"
-          />
-          <Link
-            href={"/"}
-            className="text-2xl md:text-4xl text-[#1d3557] font-semibold flex items-center ml-2 md:ml-4"
-            style={{ textDecoration: 'none' }} 
-          >
-            PORTFOLIO
-          </Link>
+    <div className="container mx-auto flex justify-between h-full items-center">
+      <div className="flex items-center">
+        <Image 
+        src="/Logo.png" 
+        alt="aboutImage"
+        width={110} 
+        height={110}
+        className="rounded-full left-0 mr-4" />
+        <Link
+          href={"/"}
+          className="text-2xl md:text-4xl text-[#1d3557] left-30 font-semibold flex items-center" 
+          style={{ textDecoration: 'none', height: '100%' }} 
+        >
+          PORTFOLIO
+        </Link>
         </div>
-        <div className="mobile-menu block md:hidden mt-3">
+     
+        <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-black text-slate-600 hover:text-black hover:border-black"
+              className="flex items-center px-3 py-2 border rounded  border-black text-slate-600 hover:text-black hover:border-black"
             >
               <Bars3Icon className="h-5 w-5" />
             </button>
@@ -65,8 +64,8 @@ const Navbar = () => {
             </button>
           )}
         </div>
-        <div className="menu hidden md:block md:w-auto" id="navbar">
-          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+        <div className="menu hidden md:block md:w-auto " id="navbar"  >
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0 ">
             {navLinks.map((link, index) => (
               <li key={index}>
                 <NavLink href={link.path} title={link.title} style={{ textDecoration: 'none' }}/>
--- app/Components/NavLink.jsx
+++ app/Components/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, title }) => {
   return (
     <Link
       href={href}
-      className="block py-2 pl-3 pr-4 text-white hover:text-[#1d3557] sm:text-2xl rounded md:p-0" style={{ textDecoration: 'none', fontSize: '1.5rem' }}
+      className="block py-2 pl-3 pr-4 text-[#457b9d] sm:text-2xl rounded md:p-0 hover:text-white" style={{ textDecoration: 'none', fontSize: '1.5rem' }}
     >
       {title}
     </Link>
